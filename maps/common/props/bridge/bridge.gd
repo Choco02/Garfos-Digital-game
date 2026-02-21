e8c03b3dbe5d900d2dcee12196c37a9e5c71a7f6
Choco02 <41271075+Choco02@users.noreply.github.com>
2026-02-21 20:02:31 -0300

    fix: esqueci de tirar o teste

--- maps/common/props/bridge/bridge.gd
+++ maps/common/props/bridge/bridge.gd
@@ -4,10 +4,6 @@ class_name Bridge
 var activated = false
 
 
-func _input(event: InputEvent) -> void:
-	interact()
-
-
 # TODO: fornecer alguma forma de ativar essa funcao
 # algum gatilho, um parent, um child, signal, chamada de funcao, event bus
 func interact():
